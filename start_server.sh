e343da602b2b6a9e43fc91b153c7fc66aa4e38a4
Weilei Zeng <240155787@qq.com>
2020-05-25 09:01:53 -0700

    rm shell scripts; clean files

--- start_server.sh
+++
@@ -1,6 +0,0 @@
-echo this script will start the server at local host. this may take 10 seconds
-
-bundle exec jekyll serve 2>&1 | grep -E -v 'deprecated|GitHub Metadata'
-
-# the last part is used to suppress warning
-# 2>&1 | grep -E -v 'deprecated|GitHub Metadata'
